4a2abb5e73792e5a3e478ad79352760503817745
jo4opaulo-rq <joaoroque317@gmail.com>
2021-07-21 23:05:28 -0300

    Bateria funcionando

---
+++ Projeto1/script.js
@@ -0,0 +1,45 @@
+document.body.addEventListener('keyup', (event) => {
+    playSound( event.code.toLowerCase() ) ;
+}); 
+
+document.querySelector('.composer button').addEventListener('click',() => {
+    let music = document.querySelector('#input').value;
+    
+    if(music !== ''){
+        let musicArray = music.split('');
+        playComposition(musicArray);
+    }
+})
+
+function playSound(sound){
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
+    if(audioElement) {
+        audioElement.currentTime = 0;
+        audioElement.play();
+    }
+
+    if (keyElement){
+        keyElement.classList.add('active');
+
+        setTimeout(()=>{
+            keyElement.classList.remove('active');
+        }, 300)
+    }
+}
+
+function playComposition(musicArray){
+
+    let wait = 0;
+
+    for(let musicItem of musicArray){
+        setTimeout(()=>{
+            playSound(`key${musicItem}`);
+        }, wait);
+        
+        wait += 245;
+
+        
+    }
+}
